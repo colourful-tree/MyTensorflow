bf477d3e8d612e2d94ec05085a070cdfb6dba6f3
colourful-tree-aliyun <colourful_tree@163.com>
2017-07-21 15:18:39 +0800

    text cnn

--- ccir/text_cnn.py
+++ ccir/text_cnn.py
@@ -29,9 +29,6 @@ class TextCNN(object):
     def __init__(
       self, query_length, doc_length, num_classes,
       embedding_size, hidden_neural_size, filter_sizes, num_filters, vocab_query, vocab_doc, l2_reg_lambda=0.0):
-
-        tf.version = tf.__version__
-        tf.version_011 = "0.11.0rc2"
         # init w2v
         self.query_vocab_size = len(vocab_query.vocabulary_)
         self.doc_vocab_size = len(vocab_doc.vocabulary_)
@@ -53,7 +50,7 @@ class TextCNN(object):
         #print self.input_y
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
-
+        self.high = 80
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
             w_query = tf.Variable(
@@ -72,149 +69,112 @@ class TextCNN(object):
                 self.embedded_chars_expanded_2 = tf.expand_dims(self.embedded_chars_2, -1, name="chars_expand_2")
                 self.embedded_fine_expanded_1 = tf.expand_dims(self.embedded_fine_1, -1, name="fine_expand_1")
                 self.embedded_fine_expanded_2 = tf.expand_dims(self.embedded_fine_2, -1, name="fine_expand_2")
-        with tf.name_scope("cat_embedding"):
-            if tf.version == tf.version_011:
+            with tf.name_scope("cat_embedding"):
                 self.embedded_expand_1 = tf.concat(3, [self.embedded_chars_expanded_1, self.embedded_fine_expanded_1], name="char_fine_1")
                 self.embedded_expand_2 = tf.concat(3, [self.embedded_chars_expanded_2, self.embedded_fine_expanded_2], name="char_fine_2")
-            else:
-                print "version error"
         # Create a convolution + maxpool layer for each filter size
-        with tf.name_scope("conv_pool"):
-            pooled_outputs_1 = []
-            for i, filter_size in enumerate(filter_sizes):
-                with tf.name_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                    filter_shape = [filter_size, embedding_size, 2, num_filters]
-                    #                                          channel
-                    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                    b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                    conv = tf.nn.conv2d(
-                        self.embedded_expand_1, #[batch=?, in_height, in_width, in_channels]
-                        W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
-                        name="conv")
-                    # Apply nonlinearity
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    #h = tf.nn.elu(tf.nn.bias_add(conv, b), name="elu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, query_length - filter_size + 1, 1, 1],
-                        #ksize=[1, query_length, embedding_size, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs_1.append(pooled)
-            pooled_outputs_2 = []
-            for i, filter_size in enumerate(filter_sizes):
-                with tf.name_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                    filter_shape = [filter_size, embedding_size, 2, num_filters]
-                    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                    b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                    #print self.embedded_expand_2
-                    #print W
-                    conv = tf.nn.conv2d(
-                        self.embedded_expand_2,
-                        W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
-                        name="conv")
-                    # Apply nonlinearity
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    #h = tf.nn.elu(tf.nn.bias_add(conv, b), name="elu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, 80, 1, 1],
-                        #ksize=[1, doc_length, embedding_size, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs_2.append(pooled)
-
-        with tf.name_scope("concat_doc"):
-            self.doc_h = tf.concat(2, pooled_outputs_2, name="doc_h")
-        #print self.doc_h
-        with tf.name_scope("conv_pool_h"):
-            pooled_outputs_h = []
-            with tf.name_scope("conv-maxpool-%s" % filter_size):
+        with tf.name_scope("query"):
+            with tf.name_scope("conv-maxpool"):
                 # Convolution Layer
-                filter_shape = [1, 1, num_filters, num_filters]
+                filter_size = 3
+                filter_shape = [filter_size, embedding_size, 2, num_filters]
+                #                                          channel
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
                 conv = tf.nn.conv2d(
-                    self.doc_h, #[batch=?, in_height, in_width, in_channels]
+                    self.embedded_expand_1, #[batch=?, in_height, in_width, in_channels]
                     W,
                     strides=[1, 1, 1, 1],
                     padding="VALID",
                     name="conv")
                 # Apply nonlinearity
+                conv = tf.nn.batch_normalization(conv, 0.001, 1.0, 0,1, 0.0001)
                 h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                #print h
                 #h = tf.nn.elu(tf.nn.bias_add(conv, b), name="elu")
                 # Maxpooling over the outputs
-                pooled = tf.nn.max_pool(
+                self.pooled_query = tf.nn.max_pool(
                     h,
-                    ksize=[1, 119, 1, 1],
+                    ksize=[1, query_length - filter_size + 1, 1, 1],
                     #ksize=[1, query_length, embedding_size, 1],
                     strides=[1, 1, 1, 1],
                     padding='VALID',
                     name="pool")
-                pooled_outputs_h.append(pooled)
+            
+            with tf.name_scope("MP"):
+                self.pooled_query = tf.nn.batch_normalization(self.pooled_query, 0.001, 1.0, 0,1, 0.0001)
+                W = tf.Variable(tf.truncated_normal([num_filters, num_filters], stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                self.query_h = tf.tanh(tf.nn.xw_plus_b(tf.squeeze(self.pooled_query), W, b, name="scores"))
+            
+            with tf.name_scope("tile"):
+                tile_cnt = doc_length - filter_size + 1 - self.high + 1
+                self.query = tf.tile(self.query_h, [1, tile_cnt], name="tile")
+                self.query = tf.reshape(self.query, [-1, tile_cnt, num_filters], name="re")
+
+        with tf.name_scope("doc"):
+            with tf.name_scope("conv-maxpool"):
+                # Convolution Layer
+                filter_size = 3
+                filter_shape = [filter_size, embedding_size, 2, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                #print self.embedded_expand_2
+                #print W
+                conv = tf.nn.conv2d(
+                    self.embedded_expand_2,
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                    name="conv")
+                # Apply nonlinearity
+                conv = tf.nn.batch_normalization(conv, 0.001, 1.0, 0,1, 0.0001)
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                #h = tf.nn.elu(tf.nn.bias_add(conv, b), name="elu")
+                # Maxpooling over the outputs
+                self.pooled_doc = tf.nn.max_pool(
+                    h,
+                    ksize=[1, self.high, 1, 1],
+                    #ksize=[1, doc_length, embedding_size, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")
+            with tf.name_scope("conv-maxpool-1"):
+                # Convolution Layer
+                filter_shape = [1, 1, num_filters, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(
+                    self.pooled_doc, #[batch=?, in_height, in_width, in_channels]
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                    name="conv")
+                # Apply nonlinearity
+                conv = tf.nn.batch_normalization(conv, 0.001, 1.0, 0,1, 0.0001)
+                self.doc = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                #print h
         
-            #print self.h_pool_1
-            #self.cnn_output_h = tf.reshape(self.h_pool_h, [-1, num_filters_total], name="cnn_output_h")
-            #print self.cnn_output_h
-        # Combine all the pooled features
-        with tf.name_scope("concat_reshape"):
-            #print pooled_outputs_1
-            print pooled_outputs_2
-            if tf.version == tf.version_011:
-                self.h_pool_1 = tf.concat(3, pooled_outputs_1, name="conv_concat_1")
-                self.h_pool_h = tf.concat(3, pooled_outputs_h, name="conv_concat_h")
-            else:
-                print "version error"
-            #print self.h_pool_1
-            self.cnn_output_1 = tf.reshape(self.h_pool_1, [-1, num_filters], name="cnn_output_1")
-            #print self.cnn_output_1
-            self.cnn_output_2 = tf.reshape(self.h_pool_h, [-1, num_filters], name="cnn_output_2")
-        #print self.cnn_output_1
-        with tf.name_scope("concat_all"):
-            if tf.version == tf.version_011:
-                #self.h_pool_flat = tf.concat(1, [self.cnn_output_1, self.cnn_sim, self.cnn_output_2, self.rnn_output_1, self.rnn_sim, self.rnn_output_2, self.overlap])
-                self.h_pool_flat_cnn = tf.concat(1, [self.cnn_output_1, self.cnn_output_2])
-            else:
-                print "version error"
-        # Add dropout
-        with tf.name_scope("dropout"):
-            #self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
-            #self.h_drop_cnn = tf.nn.dropout(self.h_pool_flat_cnn, self.dropout_keep_prob)
-            self.h_drop_cnn = self.h_pool_flat_cnn
-        #print self.h_pool_flat
-        #print self.cnn_output_1
-        #print self.h_drop_cnn
-        
+        with tf.name_scope("Hadamand"):
+            #self.hadamand = tf.mul(self.query, tf.squeeze(self.doc))    
+            #self.query = tf.expand_dims(self.query, -1, name="expand_query")
+            self.doc = tf.squeeze(self.doc, [2])
+            #self.doc = tf.expand_dims(self.doc, -1)
+            self.hadamand = tf.mul(self.query, self.doc)
+            self.hadamand = tf.reshape(self.hadamand, [-1, num_filters * (doc_length - filter_size + 1 - self.high + 1)]) 
         # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
-            """W = tf.Variable(tf.truncated_normal([num_filters_total * 2 + hidden_neural_size * 2 + 2 + 1, num_classes], stddev=0.1),
-                #tf.random_uniform([num_filters_total * 2 + hidden_neural_size * 2 + 2 + 1, num_classes], -1.0, 1.0),
-                name="W")"""
             """
-            W = tf.get_variable("W", shape=[num_filters_total * 2 + hidden_neural_size * 2 + 2 + 1, num_classes], initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            W = tf.get_variable("W", shape=[(doc_length - filter_size + 1 - self.high + 1) * num_filters, num_filters], initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+            self.scores = tf.nn.relu(tf.nn.xw_plus_b(self.hadamand, W, b, name="scores"))
+            W_2 = tf.get_variable("W_2", shape=[num_filters, 3], initializer=tf.contrib.layers.xavier_initializer())
+            b_2 = tf.Variable(tf.constant(0.1, shape=[3]), name="b_2")
+            self.scores = tf.nn.xw_plus_b(self.scores, W_2, b_2, name="scores")
             """
-            W = tf.get_variable("W", shape=[num_filters * 2, num_classes], initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            self.scores = tf.nn.xw_plus_b(self.h_drop_cnn, W, b, name="scores")
-            #self.predictions = tf.argmax(self.scores, 1, name="predictions")
-            #self.distance = tf.sqrt(tf.reduce_sum(tf.square(tf.sub(self.cnn_output_1,self.cnn_output_2)),1,keep_dims=True))
-            #self.distance = tf.div(self.distance, tf.add(tf.sqrt(tf.reduce_sum(tf.square(self.cnn_output_1),1,keep_dims=True)),tf.sqrt(tf.reduce_sum(tf.square(cnn_output_2),1,keep_dims=True))))
-            #self.distance = tf.reshape(self.distance, [-1], name="distance")
+            self.hadamand = tf.nn.batch_normalization(self.hadamand, 0.001, 1.0, 0,1, 0.0001)
+            W = tf.get_variable("W", shape=[(doc_length - filter_size + 1 - self.high + 1) * num_filters, 3], initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[3]), name="b")
+            self.scores = (tf.nn.xw_plus_b(self.hadamand, W, b, name="scores"))
 
         # CalculateMean cross-entropy loss
         with tf.name_scope("loss"):
